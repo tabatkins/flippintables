5bf851ac2817ddca376c14959fccbe35f1ca0941
Tab Atkins <jackalmage@gmail.com>
2011-12-07 15:56:42 -0800

    Fix to actually work now that I've pulled out the authData, and to add the ability to reply.

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 include "include.php";
 
-function post_tweet($tweet_text) {
+function post_tweet($authData, $data) {
 	$connection = new tmhOAuth($authData); 
-
-	$connection->request('POST', 
-		$connection->url('1/statuses/update'), 
-		array('status' => $tweet_text));
-	
+	$connection->request('POST', $connection->url('1/statuses/update'), $data);
 	return $connection->response['code'];
 }
 
@@ -16,7 +12,12 @@ if( $_POST['tweet'] ) {
 	$emotion = substr($text, -1) == "?" ? "confusion" : "anger";
 	$text = trim($_POST['tweet'], "?!") . ($emotion == "confusion" ? "?!?" : "!!!") .  "\n(╯°□°)╯︵ ┻━┻";
 	$text = strtoupper($text);
-	$result = post_tweet($text);
+
+	if( trim($_POST['reply-to']) == "") {
+		$result = post_tweet($authData, array('status'=>$text));		
+	} else {
+		$result = post_tweet($authData, array('status'=>$text, 'in_reply_to_status_id'=>trim($_POST['reply-to'])));
+	}
 }
 ?>
 <!DOCTYPE html>
@@ -26,5 +27,6 @@ if( $_POST['tweet'] ) {
 	<p>Tweet sent, response was <?= $result ?>.</p>
 <?php endif; ?>
 <form method=post>
+	<p>Replying to: <input name=reply-to></p>
 	<input size=124 maxlength=124 name=tweet autofocus><button type=submit>(╯°□°)╯︵ ┻━┻</button>
 </form>
